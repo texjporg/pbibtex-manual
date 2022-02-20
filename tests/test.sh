3a797713a8f06243636c43ecad4c613b21e1c5a9
Hironobu Yamashita <h.y.acetaminophen@gmail.com>
2022-02-20 10:33:55 +0900

    add tests/03substring

--- tests/test.sh
+++ tests/test.sh
@@ -32,7 +32,7 @@ uplatex -kanji=utf8 01substring-up
 dvipdfmx 01substring-up
 diff 01substring-up.bbl.base 01substring-up.bbl || exit 1
 
-### 02j: line break in BBL output
+### 02jnewline: line break in BBL output
 
 ptex -kanji=utf8 02jnewline
 pbibtex -kanji=utf8 02jnewline
@@ -47,5 +47,23 @@ uptex -kanji=utf8 02jnewline
 uptex -kanji=utf8 02jnewline
 dvipdfmx 02jnewline
 diff 02jnewline.bbl.base 02jnewline.bbl || exit 1
+
+### 03substring: substring$
+
+ptex -kanji=utf8 03substring-p
+pbibtex -kanji=utf8 03substring-p
+ptex -kanji=utf8 03substring-p
+ptex -kanji=utf8 03substring-p
+dvipdfmx 03substring-p
+diff 03substring-p.bbl.base 03substring-p.bbl || exit 1
+
+uptex -kanji=utf8 03substring-up
+upbibtex -kanji=utf8 03substring-up
+uptex -kanji=utf8 03substring-up
+uptex -kanji=utf8 03substring-up
+dvipdfmx 03substring-up
+diff 03substring-up.bbl.base 03substring-up.bbl || exit 1
+
+### 03jtest: not working now!
 
 echo SUCCESS
