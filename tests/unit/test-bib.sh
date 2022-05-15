a9fef4355bde742f435f2d8ee678677c6ba0a0a3
TANAKA Takuji <ttk@t-lab.opal.ne.jp>
2022-05-15 19:40:39 +0900

    tests/unit/*: add unit tests (#2)
    *.bbl.*base は TeX Live svn r63305 の pbibtex, upbibtex, bibtexu で得られたものです。

---
+++ tests/unit/test-bib.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+#
+# Copyright 2022 TANAKA Takuji <ttk@t-lab.opal.ne.jp>
+# You may freely use, modify and/or distribute this file.
+
+#SOURCE=somewhere/source
+PBIBTEX=$SOURCE/Work/texk/web2c/pbibtex
+UPBIBTEX=$SOURCE/Work/texk/web2c/upbibtex
+UPBIBTEX_EUC=$SOURCE/Work/texk/web2c/upbibtex\ -kanji-internal=euc
+BIBTEXU=$SOURCE/Work/texk/bibtex-x/bibtexu
+
+TEXMFCNF=$SOURCE/texk/kpathsea
+export TEXMFCNF
+
+rc=0
+bp=
+bu=
+be=
+bv=
+rm -f *.bbl *.blg
+
+### pbibtex
+
+for job in casep periodp substrp txtprfxp widthp namep isknjp charp; do
+
+max_print_line=79 $PBIBTEX $job \
+  && diff $job.bbl.base $job.bbl || { rc=1 ; bp=$bp,$job ; }
+
+done
+
+
+### upbibtex
+
+for job in caseu periodu substru txtprfxu widthu nameu isknju charu; do
+
+max_print_line=79 $UPBIBTEX $job \
+  && diff $job.bbl.base $job.bbl || { rc=2 ; bu=$bu,$job ; }
+
+done
+
+
+### upbibtex -kanji-internal=euc
+
+for job in casep periodp substrp txtprfxp widthp namep isknjp charp; do
+
+max_print_line=79 $UPBIBTEX_EUC $job \
+  && diff $job.bbl.base $job.bbl || { rc=3 ; be=$be,$job ; }
+
+done
+
+
+### bibtexu
+
+for job in caseu periodu substru txtprfxu widthu nameu isknju charu; do
+
+max_print_line=79 $BIBTEXU $job \
+  && diff $job.bbl.ubase $job.bbl || { rc=4 ; bv=$bv,$job ; }
+
+done
+
+
+if [ $rc -gt 0 ]; then
+  if [ -n "$bp" ]; then
+    echo ERROR in pbibtex $bp
+  fi
+  if [ -n "$bu" ]; then
+    echo ERROR in upbibtex $bu
+  fi
+  if [ -n "$be" ]; then
+    echo ERROR in \"upbibtex -kanji-internal=euc\" $be
+  fi
+  if [ -n "$bv" ]; then
+    echo ERROR in bibtexu $bv
+  fi
+else
+  echo SUCCESS
+fi
+exit $rc
+
